87d070d0979753a93b6b9df8b7c449d440b6f213
vasanth123-agri <vasanth@agriinverse.in>
2025-10-22 16:23:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json()):
 const desiredOrder = ["FId","Id","UId","BId","CO2","WS","PAR","CT","CH","N","P","K","SM","ST","EC","F1","F2","F3","F4","F5","F6","F7","F8","Clear","NIR"];
 
 app.post('/log', (req, res) => {
@@ -9,4 +13,7 @@ app.post('/log', (req, res) => {
     });
     console.log('Ordered data:', orderedData);
     res.json({ status: 'success', received: orderedData });
+});
+app.listen(port, () => {
+    console.log(`Railway server running on port ${port}`);
 });
